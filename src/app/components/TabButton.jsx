7bd9318bc879bcd5e6211595945c6e7ae77886c9
B NIKHIL KRISHNA <162432391+NikhilKrishna2003@users.noreply.github.com>
2024-03-26 11:32:56 +0530

    Update TabButton.jsx

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react"
 
 const TabButton = ({active, selectTab, children}) => {
-const buttonClasses = active ? 'text-white border-b button-green-500' : 'tect-[#AD7BE]'  
+const buttonClasses = active ? "text-white border-b button-green-500" : "tect-[#AD7BE]"  
   return (
     <button onClick={selectTab}> 
      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
