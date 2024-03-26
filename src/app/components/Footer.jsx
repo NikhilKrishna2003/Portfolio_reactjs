241bcd73004eb73d90473b4fe9cba6d6bc3bf264
B NIKHIL KRISHNA <162432391+NikhilKrishna2003@users.noreply.github.com>
2024-03-26 11:29:21 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"
 
 const Footer = () => {
   return (
-    <footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
-            <div className='container p-12 flex justify-between'>
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+            <div className="container p-12 flex justify-between">
                 <span>LOGO</span>
-                <p className='text-slate-600'>All right</p>
+                <p className="text-slate-600">All right</p>
             </div>
     </footer>
   )
